ec82be79def5e9e4485173424d4baf07a1daa7e3
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-08-22 17:53:14 +0600

    Server setup done

---
+++ index.js
@@ -0,0 +1,57 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(cors());
+app.use(express.json())
+const port = process.env.PORT || 5000
+
+const client = new MongoClient(process.env.MONGODV_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const database = client.db("BuySell");
+        const ProductColllection = database.collection("Product");
+
+        app.post("/product", async (req, res) => {
+            const data = req.body;
+            const result = await ProductColllection.insertOne(data);
+            res.send(result)
+        });
+        app.get("/product", async (req, res) => {
+            const result = await ProductColllection.find().toArray();
+            res.send(result)
+        })
+        app.get("/product/:id", async (req, res) => {
+            const { id } = req.params;
+            const product = await ProductColllection.findOne({ _id: new ObjectId(id) });
+
+            if (!product) {
+                return res.status(404).json({ message: "Product not found" });
+            }
+            res.json(product);
+        });
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
